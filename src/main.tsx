9f21270cfd0189792f1f6e6e36474f52fba5320c
Yuri <1yuricoutinho@gmail.com>
2026-07-20 08:30:13 -0300

    feat: configure design system tokens and global styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 import { queryClient } from './config/queryClient';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
